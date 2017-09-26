54670f23329c951767c3e340f8f67ef618fa8ddf
Mason Galland <masongalland@me.com>
2017-09-26 10:07:47 -0600

    finished destructuring from js3

--- afternoon-projects/1.3/javascript-iii-master/destructuring.js
+++ afternoon-projects/1.3/javascript-iii-master/destructuring.js
@@ -12,6 +12,7 @@ var carDetails = {
 }
 
 // CODE HERE
+var {color, make, model, year} = carDetails;
 
 
 // ========================
@@ -21,6 +22,7 @@ var carDetails = {
 
 function greeting( obj ) {
   // CODE HERE
+  var {firstName, lastName, title} = obj
   
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
 }
@@ -32,6 +34,10 @@ function greeting( obj ) {
 // Write a function called totalPopulation that will take in an object. That object will have 4 properties named utah, california, texas and arizona. The property values will be numbers. Use object destructuring to save the property values to new variables. Sum up the values and return the total number.
 
   // CODE HERE
+  function totalPopulation(obj){
+    var {utah, california, texas, arizona} = obj;
+    return utah + california + texas + arizona;
+  }
 
 
 // ========================
@@ -40,7 +46,12 @@ function greeting( obj ) {
 // Write a function called ingredients that will take in an object. This object will have 3 properties named carb, fat, and protein. The property values will be strings. Use object destructuring to save the property values to new variables. Push these new variables to an array and return the array. 
 
   // CODE HERE
-
+  function ingredients(obj){
+    var {carb, fat, protein} = obj;
+    var arr = [];
+    arr.push(carb, fat, protein);
+    return arr;
+  }
 
 // ========================
 
@@ -54,7 +65,9 @@ function greeting( obj ) {
 // Write a function called largeNumbers that will take a destructured object as it's parameter. The object properties will be named first, second, and third and their values will be numbers. Find the smallest number of the three and return that number.
 
   // CODE HERE
-
+  function largeNumbers({first, second, third}){
+    return Math.min(first, second, third)
+  }
 
 // ========================
 
@@ -62,3 +75,7 @@ function greeting( obj ) {
 // Write a function called numberGroups that will take a destructured object as it's parameter. The object properties will be named a, b, and c and their values will be arrays of numbers. Find the longest array and return that array. 
 
   // CODE HERE
+  function numberGroups({a, b, c}){
+    var max = Math.max(a.length, b.length, c.length);
+    return a.length === max ? a : b.length === max ? b : c
+  }
