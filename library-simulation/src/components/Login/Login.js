38fc6aea8c9f8afc153081e676c542610d4ed955
masongalland <masongalland@me.com>
2017-12-13 11:35:24 -0700

    finished styling for login page on library sim

--- library-simulation/src/components/Login/Login.js
+++ library-simulation/src/components/Login/Login.js
@@ -1,20 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import './login.css';
-import logo from '../../assets/maroon-logo.svg';
+import "./login.css";
+import logo from "../../assets/maroon-logo.svg";
 
 export default class Login extends Component {
-    render() {
-        return (
-            <div className="login-wrapper" >
-                <img src={logo} />
-                <h2>Book Exchange</h2>
-                <div className="login-form">
-                    <input ref="name" type="text" />
-                    <input ref="pass" type="text" />
-
-                </div>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className="login-wrapper">
+				<img src={logo} />
+				<h2>Book Exchange</h2>
+				<div className="login-form">
+					<div className="login-input">
+						<label>Username</label>
+						<input ref="name" type="text" />
+					</div>
+					<div className="login-input">
+						<label>Password</label>
+						<input ref="pass" type="text" />
+					</div>
+                    <div>
+                        <button>Register</button>
+                        <button>Login</button>
+                    </div>
+				</div>
+			</div>
+		);
+	}
 }
